122a8dc95fadb7f5e407a710a8f72c1b83a7c4fc
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2015-11-14 18:32:35 -0800

    adding changes from day2

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,13 +1,18 @@
 Given(/^a "([^"]*)" request is made to "([^"]*)"$/) do |method, path|
+  # construct URI with endpoint i.e http://api.bayqatraining.com/user
   @uri    = URI("http://api.bayqatraining.com#{path}")
+  # @method is a variable that reference to the verb (i.e GET, POST)
   @method = method
 end
 
 When(/^these parameters are supplied in URL:$/) do |table|
+  # flattens table parameters from feature file
+  # into URI format i.e email=andrei9%40gmail.com&password=secret
   @uri.query = URI.encode_www_form(table.rows_hash)
 end
 
-Then(/^the api call should succeed$/) do
+Then(/^the api call should (succeed|fail)$/) do | condition |
+  # picks which http verb to use bases on @method variable value
   case @method.downcase
     when 'get'
       method = Net::HTTP::Get
@@ -19,9 +24,44 @@ Then(/^the api call should succeed$/) do
       method = Net::HTTP::Delete
   end
 
+  # start method immediately creates a connection to an HTTP server
+  # which is kept open for the duration of the block
   Net::HTTP.start(@uri.host, @uri.port) do |http|
+    # line 32 declared variable request and fire a call; see next line for flattened call
+    # Net::HTTP::Get.new(http://api.bayqatraining.com/login?email=andrei9%40gmail.com&password=secret)
     request   = method.new(@uri)
     @response = http.request request
   end
-  puts @response.body
+  # puts @response.body
+
+  # value of condition variable can be only succeed or failed which we specify in feature file
+  case condition
+    when 'succeed'
+      # raise error message in terminal if response has error and response is NOT successful
+      if !@response.is_a?(Net::HTTPSuccess) || @response.body['Error']
+        raise 'Request failed, expected success'
+      end
+    when 'fail'
+      # raise error message in terminal if response does NOT have error and response IS successful
+      if @response.is_a?(Net::HTTPSuccess) && !@response.body['Error']
+        raise 'Request succeeded, expected failure'
+      end
+  end
 end
+
+
+And(/^these response keys should have value:$/) do |table|
+  # Parse the JSON document _source_ into a Ruby HashMap data structure and return it
+  # i.e table = {"id"=>108, "name"=>"Andrei", "email"=>"andrei9@gmail.com"}
+  @parsed_response = JSON.parse(@response.body)
+  table.raw.each do |row|
+    # go through each row in table from feature file
+    puts row
+    # match that first row from server response is equal to first row from table in feature file
+    # name"=>"Andrei" == "name", "Andrei"
+    expect(@parsed_response[row[0]]).to be == row[1]
+  end
+end
+
+
+
--- features/user.feature
+++ features/user.feature
@@ -7,8 +7,47 @@ Feature: Test scenarios for user API
   Scenario: Create new user with valid student details
     Given a "POST" request is made to "/users"
     When these parameters are supplied in URL:
-      | name     | Andrei Antipin   |
-      | email    | andrei@gmail.com |
-      | password | secret           |
+      | name     | Andrei Antipin    |
+      | email    | andrei9@gmail.com |
+      | password | secret            |
     Then the api call should succeed
+    And these response keys should have value:
+      | name  | Andrei Antipin    |
+      | email | andrei9@gmail.com |
+
+  Scenario Outline: Create new user with invalid student details
+    Given a "POST" request is made to "/users"
+    When these parameters are supplied in URL:
+      | name     | <name>     |
+      | email    | <email>    |
+      | password | <password> |
+    Then the api call should fail
+    And these response keys should have value:
+      | Error | <error> |
+  Examples:
+    | name          | email               | password | error                                  |
+    |               | andrei888@gmail.com | secret   | Name can't be blank                    |
+    | Andrei Antipi |                     | secret   | Email can't be blank, Email is invalid |
+#    | name | email            | password | error |
+#    | name | email            | password | error |
+#    | name | email            | password | error |
+#    | name | email            | password | error |
+
+  Scenario: Login with valid user details
+    Given a "POST" request is made to "/login"
+    When these parameters are supplied in URL:
+      | email    | andrei9@gmail.com |
+      | password | secret            |
+    Then the api call should succeed
+    And these response keys should have value:
+      | name  | Andrei            |
+      | email | andrei9@gmail.com |
+
+
+
+
+
+
+
+
 
